d979f95c8e3149e2b6f3d89ecf783695d2cb38cf
gimen <gimenamedrano2003@outlook.com>
2025-10-30 19:09:47 -0600

    CustomerLogin componentes 8
    Signed-off-by: gimen <gimenamedrano2003@outlook.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from './components/Footer.jsx';
 import Home from './pages/Home.jsx';
 import Menu from './pages/Menu.jsx';
 import Contact from './pages/Contact.jsx';
-import EmployeeLogin from './components/Employee/EmployeeLogin.jsx';
+
 import Reservations from './pages/Reservations.jsx';
 import BackgroundVideo from './components/BackgroundVideo.jsx';
 import AuthCallback from "./pages/AuthCallback.jsx";
@@ -16,14 +16,12 @@ export default function App() {
             <div className="app-container">
 
                 <BackgroundVideo />
-
-
                 <Header />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/menu" element={<Menu />} />
                     <Route path="/contacto" element={<Contact />} />
-                    <Route path="/empleados" element={<EmployeeLogin />} />
+
                     <Route path="/reservas" element={<Reservations />} />
                     <Route path="/auth/callback" element={<AuthCallback />} />
                     <Route path="*" element={<Home />} />
